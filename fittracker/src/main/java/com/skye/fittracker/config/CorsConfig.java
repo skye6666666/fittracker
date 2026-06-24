811ffb8d01413414a10ab5930f6050726dc62aa1
SKYE <dannymacaskill510@yahoo.com.tw>
2026-06-24 17:46:05 +0800

    add cors setting

--- fittracker/src/main/java/com/skye/fittracker/config/CorsConfig.java
+++ fittracker/src/main/java/com/skye/fittracker/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:5173","https://fittracker-fw5g.vercel.app")
         );
 
         configuration.setAllowedMethods(
