e876aec09d44e04341f3cf470651a6a55f45b347
SKYE <dannymacaskill510@yahoo.com.tw>
2026-06-25 12:05:43 +0800

    replace SMTP with Resend email API

--- fittracker/src/main/java/com/skye/fittracker/service/EmailService.java
+++ fittracker/src/main/java/com/skye/fittracker/service/EmailService.java
@@ -1,42 +1,43 @@
 package com.skye.fittracker.service;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    @Value("${RESEND_API_KEY}")
+    private String apiKey;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+    private final RestTemplate restTemplate = new RestTemplate();
 
-    public void sendVerificationEmail(
-            String to,
-            String token
-    ) {
+    public void sendVerificationEmail(String to, String token) {
 
-        String verifyUrl =
-//                "http://localhost:8080/api/users/verify?token="
-                "https://fittracker.vercel.app/verify?token="
-                        + token;
+        String url = "https://api.resend.com/emails";
 
-        SimpleMailMessage message =
-                new SimpleMailMessage();
+        String verifyLink =
+                "https://fittracker-9sb7.onrender.com/api/users/verify?token=" + token;
 
-        message.setTo(to);
-
-        message.setSubject(
-                "Verify your FitTracker account"
+        Map<String, Object> request = Map.of(
+                "from", "FitTracker <onboarding@resend.dev>",
+                "to", new String[]{to},
+                "subject", "Verify your email",
+                "html", "<h2>Welcome to FitTracker</h2>"
+                        + "<p>Click below to verify:</p>"
+                        + "<a href=\"" + verifyLink + "\">Verify Email</a>"
         );
 
-        message.setText(
-                "Click the link below:\n\n"
-                        + verifyUrl
-        );
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + apiKey);
 
-        mailSender.send(message);
+        HttpEntity<Map<String, Object>> entity =
+                new HttpEntity<>(request, headers);
+
+        restTemplate.postForEntity(url, entity, String.class);
     }
 }
